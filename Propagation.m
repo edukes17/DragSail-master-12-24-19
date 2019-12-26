c9111fcf2efcc79e852dc8149c56ad76842393ae
edukes17 <56701030+edukes17@users.noreply.github.com>
2019-12-26 15:27:04 -0500

    single timeseries
    use the updated propagation.m file with a single timeseries

--- Propagation.m
+++ Propagation.m
@@ -1,15 +1,14 @@
 function [ Time_history, State_history ] = Propagation( Windtunnel, State_initial, Orientation_initial, Time_cell, Tolerance, Deorb_Alt, Spacecraft, Surface, outdir, OS)
 %% Propagation.m function
 
-% Purpose: Propagate the attitude and trajectory of the spacecraft in orbit.
+%Purpose: Propagate the attitude and trajectory of the spacecraft in orbit.
  
-% Created:  Sebastian Tamrazian 12/23/2018
-% Modified: Sebastian Tamrazian 2/12/2019 
-% Modified: Arly Black 9/24/2019
-% Modified: Arly Black 11/25/2019
+%Created:  Sebastian Tamrazian 12/23/2018
+%Modified: Sebastian Tamrazian 2/12/2019 
+%Modified: Arly Black 9/24/2019
  
-% Acknowledgement: Jay Iuliano for his excellent help and knowledge of orbit
-% propagation methods
+%Acknowledgement: Jay Iuliano for his excellent help and knowledge of orbit
+%propagation methods
 
 %Inputs:
 %     Windtunnel: 1 or 0 to specify if the mode is active/inactive
@@ -45,11 +44,10 @@ function [ Time_history, State_history ] = Propagation( Windtunnel, State_initia
 %                     1. .mass : sc mass [kg]
 %                     2. .inertia: spacecraft inertia [kg*m^2]
 %                     3. .cm : spacecraft cm relative to geom origin [m]
-%                     4. .frontal_area : spacecraft frontal area [m^2]
-%                     5. .centroids : centroid locations of sc surface elements [m]
-%                     6. .normals : normals of sc surface elements (unit vectors) []
-%                     7. .areas : areas of sc surface elements [m^2]
-%                     8. .tangents : tangents of sc surface elements (unit vectors)[]
+%                     4. .centroids : centroid locations of sc surface elements [m]
+%                     5. .normals : normals of sc surface elements (unit vectors) []
+%                     6. .areas : areas of sc surface elements [m^2]
+%                     7. .tangents : tangents of sc surface elements (unit vectors)[]
 %
 %     Surface: structure with fields:
 %                     1. .temperature : sc surface temperature [K]
@@ -59,8 +57,6 @@ function [ Time_history, State_history ] = Propagation( Windtunnel, State_initia
 %                     5. .back_emiss_coef : back emissivity coefficient - between 0 and 1 []
 %                     6. .front_nonLamb_coef : front non-Lambertian coefficient - between 0 and 1 []
 %                     7. .back_nonLamb_coef : back non-Lambertian coefficient - between 0 and 1 []
-%                     8. .transmissivity_coef = Transmissivity for use with clear sail
-%                     9. .Refl                = Specify clear or reflecting surface 1=Refl, 0=Clear
 %
 %     outdir: specifies output file path for simulation results
 %
@@ -128,139 +124,36 @@ if ~Windtunnel
     [R_sun] = SunPosition(Time_epoch);
  
     %define timeseries to save parameters    
-    global ts1
-    global ts2
-    global ts3
-    global ts4
-    global ts5
-    global ts6
-    global ts7
-    global ts8
-    global tsc9
-    global ts9
-    global tsf1
-    global tsf2
-    global tsf3
-    global tsf4
-    global tst1
+   
+    global tsAll
     
-    ts1 = timeseries('AeroTorques');
-    ts2 = timeseries('AeroForces');
-    ts3 = timeseries('GravTorques');
-    ts4 = timeseries('SolarTorques');
-    ts5 = timeseries('SolarForces');
-    ts6 = timeseries('SunBody');
-    ts7 = timeseries('Cd');
-    ts8 = timeseries('Cl');
-    ts9 = timeseries('Euler');
-    tsf1 = timeseries('alpha');
-    tsf2 = timeseries('beta');
-    tsf3 = timeseries('aoa');
-    tsf4 = timeseries('flow');
-    tst1 = timeseries('Torque');
+    tsAll = timeseries('AllStuff');
     
-    
+    profile on 
+   
     %Propagate
     [Time_history,State_history] = ode113(@(Time, State) Orbitalcase(Time, State, Spacecraft, Surface, EARTH, Time_epoch, R_sun),...
                                             [0 Time_propagation], SixDOF_initial, options);
+    profile viewer
 
     State_history = real(State_history);
     
-
-    % Pre-allocate variable size
-    zeros3 = zeros(3,length(State_history));
-    Torques_aero = zeros3;
-    Forces_aero  = zeros3;
-    Torques_srp  = zeros3;
-    Forces_srp   = zeros3;
-    Torques_grav = zeros3;
-    Forces_grav  = zeros3;
-    alpha        = zeros3(1,:);
-    beta         = zeros3(1,:);
-    aoa         = zeros3(1,:);
-    Vrel_body    = zeros3;
-    SB           = zeros3;
-    Cdrag        = zeros3(:,1);
-    Clift        = zeros3(:,1);
-    
-    % Calculate forces and torques for given state history
-    for nn = 1:(length(State_history))
-        r = State_history(nn, 1:3);             % position [m]  
-        v = State_history(nn, 4:6);             % velocity [m/s]
-        q = State_history(nn, 7:10);            % quaternions, q4 scalar [none]
-        
-               
-        Freestream = AtmosphericProperties(EARTH, Time_epoch, State_history(nn,1:3)', State_history(nn,4:6)'); % freestream properties
-        Euler_angles = Convert_EP2EA(q(1:4));                   % convert from quaternions to euler angles
-        BODY2ECI = Convert_BODY2ECI(Euler_angles);              % convert from body to ECI frame
-        Shadow = eclipseCheck(R_sun,r',EARTH);                  % check if s/c is in eclipse (for SRP calcs)
-        vr_body = BODY2ECI'*Freestream.velocity;
-        alpha(1,nn) = atan2(vr_body(3),vr_body(1));             % calculate angle of attack [rad]
-        beta(1,nn) = atan2(vr_body(2),vr_body(1));              % calculate side slip angle [rad]
-        vrhat = vr_body/norm(vr_body);
-        aoa(1,nn) = acos(dot(vrhat,[1,0,0]));              % total angle of attack [rad] (angle bw vr and x axis)
-        
-        % Calculate perturbations
-        [~,Torque_aero,Force_aero,Cd,Cl] = Perturbations_Aerodynamics(Spacecraft, Freestream, Surface, BODY2ECI); 
-        [~, Torque_grav,Force_asph] = Perturbations_Gravitational(r', EARTH, Spacecraft, BODY2ECI);
-        [~, Torque_srp,Force_srp,R_sun_hat] = Perturbations_SolarRadiationPressure(Spacecraft, r', R_sun, Surface, Shadow, BODY2ECI);
-        
-        % Store values
-        Torques_aero(:,nn) = Torque_aero';
-        Forces_aero(:,nn)  = Force_aero';
-        Torques_grav(:,nn) = Torque_grav';
-        Forces_grav(:,nn)  = Force_asph';
-        Torques_srp(:,nn)  = Torque_srp';
-        Forces_srp(:,nn)   = Force_srp';
-        Vrel_body(:,nn)    = vr_body;
-        SB(:,nn)           = R_sun_hat';          % Sun-in-body vector
-        Cdrag(nn,1)        = Cd;
-        Clift(nn,1)        = Cl;
-        
-    end
-       
-    Euler_history = rad2deg(Convert_EP2EA(State_history(:,7:10)));
-    ang_of_attack = rad2deg(alpha');
-    sideslip = rad2deg(beta');
-    aoa_tot = rad2deg(aoa');
-    
-    vr = Vrel_body';
-    F_grav = Forces_grav';
-    T_grav = Torques_grav';
-    F_srp = Forces_srp';
-    T_srp = Torques_srp';
-    Sun_body = SB';
-    F_aero = Forces_aero';
-    T_aero = Torques_aero';
-    
     %% Output results
     
-    T1 = table(tsf1.time,tsf1.data,tsf2.data,tsf3.data,'VariableNames',{'Time','Alpha','Beta','AOA'});
-    T2 = table(ts9.time,ts9.data,'VariableNames',{'Time','Euler'});
-    T3 = table(ts1.time,ts1.data,ts2.data,'VariableNames',{'Time','AeroT','AeroF'});
-    T4 = table(ts7.time,ts7.data,'VariableNames',{'Time','Cd'});
-% Set the time interval for calculating the average Cd    
-        if Time_propagation > 1000      
-           k = int8(Time_propagation/500);
-        else
-           k=10; 
-        end   
-    Cd_ave = movmean(T4.('Cd'),k,'SamplePoints',T4.('Time'));
-    T4 = table(ts7.time,ts7.data,Cd_ave,ts8.data,'VariableNames',{'Time','Cd','Cd_ave','Cl'});
-    T5 = table(ts4.time,ts4.data,ts5.data,ts6.data,'VariableNames',{'Time','SolarT','SolarF','SunBody'});
-    T6 = table(ts3.time,ts3.data,'VariableNames',{'Time','GravT'});
-    
-    
+    [T1,T2,T3,T4,T5,T6,T7,T8]=UnpackSavedData(tsAll,Time_propagation);
+   
+        
     % Check if user OS is PC or Mac, then output results to indicated folder
     if ispc 
         % For PC
-        writetable(T1,[pwd '\' outdir '\AtmosAngles.csv']);
-        writetable(T2,[pwd '\' outdir '\Euler.csv']);
-        writetable(T3,[pwd '\' outdir '\Aero.csv']);
-        writetable(T4,[pwd '\' outdir '\Coeffs.csv']);
-        writetable(T5,[pwd '\' outdir '\Solar.csv']);
-        writetable(T6,[pwd '\' outdir '\Grav.csv']);
-        
+      writetable(T1,[pwd '\' outdir '\AtmosAngles.csv']);
+      writetable(T2,[pwd '\' outdir '\Euler.csv']);
+      writetable(T3,[pwd '\' outdir '\Aero.csv']);
+      writetable(T4,[pwd '\' outdir '\Coeffs.csv']);
+      writetable(T5,[pwd '\' outdir '\Solar.csv']);
+      writetable(T6,[pwd '\' outdir '\Grav.csv']);
+%      writetable(T7,[pwd '\' outdir '\All.csv']);
+      writetable(T8,[pwd '\' outdir '\TotalForces.csv']);
     elseif ismac
         % For Mac
         writetable(T1,[pwd '/' outdir '/AtmosAngles.csv']);
@@ -269,12 +162,12 @@ if ~Windtunnel
         writetable(T4,[pwd '/' outdir '/Coeffs.csv']);    
         writetable(T5,[pwd '/' outdir '/Solar.csv']);
         writetable(T6,[pwd '/' outdir '/Grav.csv']);
+        writetable(T7,[pwd '/' outdir '/All.csv']);
+        writetable(T8,[pwd '/' outdir '/TotalForces.csv']);
     end
     
     % Plot results
-
-   PlotResults(Time_history, State_history,R_sun,Orientation_initial,EARTH,T2,T3,T4,outdir);
-
+    PlotResults(Time_history, State_history,R_sun,Orientation_initial,EARTH,T1,T2,T3,T4,T5,T6,T8,outdir);
 
 end
 end
@@ -284,21 +177,7 @@ end
 
  function [Statederivative] = Orbitalcase(Time, State, Spacecraft, Surface, EARTH, Time_epoch, R_sun)
           
-   global ts1
-   global ts2
-   global ts3
-   global ts4
-   global ts5
-   global ts6
-   global ts7
-   global ts8
-   global tsc9
-   global ts9
-   global tsf1
-   global tsf2
-   global tsf3
-   global tsf4
-   global tst1
+   global tsAll
    
  % Translational state
     r = State(1:3); %position [m]  
@@ -310,53 +189,33 @@ end
     Euler_angles = Convert_EP2EA(q(1:4).');
     [BODY2ECI] = Convert_BODY2ECI(Euler_angles);    % body to ECI coordinate conversion
     Euler = rad2deg(Euler_angles);
-    ts9 = addsample(ts9,'Data',Euler,'Time',Time,'OverwriteFlag',true);
         
     % Obtain freestream conditions and earth shadow for current state
     [Freestream] = AtmosphericProperties(EARTH, Time_epoch, r, v);
     vr_body = BODY2ECI.'*Freestream.velocity;
-    alpha = atan2(vr_body(3),vr_body(1));             % calculate angle of attack [rad]
-    ang_of_attack = rad2deg(alpha.');
-    beta = atan2(vr_body(2),vr_body(1));              % calculate side slip angle [rad]
-    sideslip = rad2deg(beta);
     vrhat = vr_body/norm(vr_body);
+    %Based on Arun's defn, alpha is the X/Y and beta is in the X/Z
+    alpha = atan2(vrhat(2),vrhat(1));             % calculate angle of attack [rad]
+    ang_of_attack = rad2deg(alpha.');
+    beta = atan2(vrhat(3),vrhat(1));              % calculate side slip angle [rad]
+    sideslip = rad2deg(beta);
+    vr_out = transpose(vrhat);
     aoa = acos(dot(vrhat,[1,0,0]));              % total angle of attack [rad] (angle bw vr and x axis)
     aoa_tot=rad2deg(aoa);
-    %Save the atmospheric data
-    tsf1 = addsample(tsf1,'Data',ang_of_attack,'Time',Time,'OverwriteFlag',true);
-    tsf2 = addsample(tsf2,'Data',sideslip,'Time',Time,'OverwriteFlag',true);
-    tsf3 = addsample(tsf3,'Data',aoa_tot,'Time',Time,'OverwriteFlag',true);
-    tsf4 = addsample(tsf4,'Data',vr_body,'Time',Time,'OverwriteFlag',true);
         
     [Shadow] = eclipseCheck(R_sun,r,EARTH);
     
     % Find  perturbations
     [Accel_aerodynamic,Torque_aerodynamic,Force_aerodynamic, Cd_tot, Cl_tot] = Perturbations_Aerodynamics(Spacecraft, Freestream, Surface, BODY2ECI);
-    %Save the results
-    Torque_aero = transpose(Torque_aerodynamic);
-    Force_aero = transpose(Force_aerodynamic);
-    ts1 = addsample(ts1,'Data',Torque_aero,'Time',Time,'OverwriteFlag',true);
-    ts2 = addsample(ts2,'Data',Force_aero,'Time',Time,'OverwriteFlag',true);
-    ts7 = addsample(ts7,'Data',Cd_tot,'Time',Time,'OverwriteFlag',true);
-    ts8 = addsample(ts8,'Data',Cl_tot,'Time',Time,'OverwriteFlag',true);
     
     [Accel_aspherical, Torque_gravity,~] = Perturbations_Gravitational(r, EARTH, Spacecraft, BODY2ECI);
-    Torque_grav = transpose(Torque_gravity);
-    ts3 = addsample(ts3,'Data',Torque_grav,'Time',Time,'OverwriteFlag',true);
     
     [Accel_srp, Torque_srp,Force_srp,R_sun_hat] = Perturbations_SolarRadiationPressure(Spacecraft, r, R_sun, Surface, Shadow, BODY2ECI);
-    Torque_solar = transpose(Torque_srp);
-    Force_solar = transpose(Force_srp);
-    Sun_body = transpose(R_sun_hat);
-    ts4 = addsample(ts4,'Data',Torque_solar,'Time',Time,'OverwriteFlag',true);
-    ts5 = addsample(ts5,'Data',Force_solar,'Time', Time,'OverwriteFlag',true);
-    ts6 = addsample(ts6,'Data',Sun_body,'Time', Time,'OverwriteFlag',true);
         
     % Sum Perturbations
     Accelerations = Accel_aerodynamic + Accel_aspherical + Accel_srp;
     Torques = Torque_aerodynamic + Torque_gravity + Torque_srp; %Torques should be a vector, was a 3x3
-    tst1 = addsample(tst1,'Data',Torques,'Time',Time,'OverwriteFlag',true);
-    
+
   % Translation :
     a = -EARTH.SGP/norm(r)^3*r + Accelerations;
        
@@ -377,6 +236,12 @@ end
 
     % 6DOF state derivative
     Statederivative = [v;a;qdot;wdot];
+
+    %   Save all of the variables 
+    Euler_out = transpose(Euler);
+%    w_out = transpose(w);
+    SaveData = [ang_of_attack;sideslip;aoa_tot;vrhat;Torque_aerodynamic;Force_aerodynamic;Cd_tot;Cl_tot;Torque_gravity;Accel_aspherical;Torque_srp;Force_srp;R_sun_hat;Torques;Accelerations;Euler_out;w];
+    tsAll = addsample(tsAll,'Time',Time,'OverwriteFlag',true,'Data',SaveData.');
  
     % Print Status
     fprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b') % delete previous time
@@ -404,3 +269,80 @@ function [value,isterminal,direction] = Deorbit(~, State, ~, ~, ~, ~, ~, ~)
     direction = 0;
 end
  
+function [T1,T2,T3,T4,T5,T6,T7,T8] = UnpackSavedData(tsAll,Time_propagation)
+
+% SaveData =
+% [ang_of_attack;sideslip;aoa_tot;vrhat;Torque_aerodynamic;Force_aerodynamic;Cd_tot;...
+% Cl_tot;Torque_gravity;Accel_aspherical;Torque_srp;Force_srp;R_sun_hat;Torques;Accelerations;Euler,w]
+% Convert timeseries to a table  
+    T7 = table(tsAll.time,tsAll.data,'VariableNames',{'Time','SaveData'});
+
+% Unpack SaveData to variables  
+    Saved_Data = T7.('SaveData');
+    Alpha_out = Saved_Data(:,1);
+    Beta_out = Saved_Data(:,2);
+    AOA_out = Saved_Data(:,3);
+    vr_out_x = Saved_Data(:,4);
+    vr_out_y = Saved_Data(:,5);
+    vr_out_z = Saved_Data(:,6);
+    Taero_out_x = Saved_Data(:,7);
+    Taero_out_y = Saved_Data(:,8);
+    Taero_out_z = Saved_Data(:,9);
+    Faero_out_x = Saved_Data(:,10);
+    Faero_out_y = Saved_Data(:,11);
+    Faero_out_z = Saved_Data(:,12);
+    Cd_out = Saved_Data(:,13);
+    Cl_out = Saved_Data(:,14);
+    Tgrav_out_x = Saved_Data(:,15);
+    Tgrav_out_y = Saved_Data(:,16);
+    Tgrav_out_z = Saved_Data(:,17);
+    ACgrav_out_x = Saved_Data(:,18);
+    ACgrav_out_y = Saved_Data(:,19);
+    ACgrav_out_z = Saved_Data(:,20);
+    Tsol_out_x = Saved_Data(:,21);
+    Tsol_out_y = Saved_Data(:,22);
+    Tsol_out_z = Saved_Data(:,23);
+    Fsol_out_x = Saved_Data(:,24);
+    Fsol_out_y = Saved_Data(:,25);
+    Fsol_out_z = Saved_Data(:,26);
+    Sbody_out_x = Saved_Data(:,27);
+    Sbody_out_y = Saved_Data(:,28);
+    Sbody_out_z = Saved_Data(:,29);
+    Tork_out_x = Saved_Data(:,30);
+    Tork_out_y = Saved_Data(:,31);
+    Tork_out_z = Saved_Data(:,32);
+    Accel_out_x = Saved_Data(:,33);
+    Accel_out_y = Saved_Data(:,34);
+    Accel_out_z = Saved_Data(:,35);
+    Euler_out_z = Saved_Data(:,36);
+    Euler_out_y = Saved_Data(:,37);
+    Euler_out_x = Saved_Data(:,38);
+    w_out_x = Saved_Data(:,39);
+    w_out_y = Saved_Data(:,40);
+    w_out_z = Saved_Data(:,41);
+    
+ % Make output tables   
+    T1 = table(T7.('Time'),Alpha_out,Beta_out,AOA_out,'VariableNames',{'Time','Alpha','Beta','AOA'});
+    T2 = table(T7.('Time'),Euler_out_z,Euler_out_y,Euler_out_x,w_out_x,w_out_y,w_out_z,'VariableNames',...
+        {'Time','Roll','Pitch','Yaw','BodyRateX','BodyRateY','BodyRateZ'});
+    T3 = table(T7.('Time'),Taero_out_x,Taero_out_y,Taero_out_z,Faero_out_x,Faero_out_y,Faero_out_z,...
+        'VariableNames',{'Time','AeroTx','AeroTy','AeroTz','AeroFx','AeroFy','AeroFz'});
+    T4 = table(T7.('Time'),Cd_out,'VariableNames',{'Time','Cd'});
+% Set the time interval for calculating the average Cd    
+        if Time_propagation > 1000      
+          k = 1000;
+        else
+           k=10; 
+        end   
+    Cd_ave = movmean(T4.('Cd'),k,'SamplePoints',T4.('Time'));
+    T4 = table(T7.('Time'),Cd_out,Cd_ave,Cl_out,'VariableNames',{'Time','Cd','Cd_ave','Cl'});
+    T5 = table(T7.('Time'),Tsol_out_x,Tsol_out_y,Tsol_out_z,Fsol_out_x,Fsol_out_y,Fsol_out_z,Sbody_out_x,...
+        Sbody_out_y,Sbody_out_z,'VariableNames',{'Time','SolarTx','SolarTy','SolarTz','SolarFx','SolarFy',...
+        'SolarFz','SunBodyX','SunBodyY','SunBodyZ'});
+    T6 = table(T7.('Time'),Tgrav_out_x,Tgrav_out_y,Tgrav_out_z,ACgrav_out_x,ACgrav_out_y,...
+        ACgrav_out_z,'VariableNames',{'Time','GravTx','GravTy','GravTz','GravAx','GravAy','GravAz'});
+    T8 = table(T7.('Time'),Tork_out_x,Tork_out_y,Tork_out_z,Accel_out_x,Accel_out_y,Accel_out_z,...
+        'VariableNames',{'Time','Torque_X', 'Torque_Y','Torque_Z','Accel_X','Accel_Z','Accel_Y'});
+   
+    
+end    
